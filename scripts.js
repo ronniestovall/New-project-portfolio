a3d61efe238e9b8ea24a66fc4998873565ca8cfd
ronniestovall <ronnie.stovall1996@gmail.com>
2017-07-17 13:59:25 -0500

    Additional changes

--- scripts.js
+++ scripts.js
@@ -7,3 +7,25 @@ function submitcontactform() {
         alert("Thank you for submittinng the form");
     }
 }
+$(Document).ready(function() {
+
+
+
+$(contactform).on('submit',submitcontactform);
+
+
+});
+
+Function submitContactform() {
+
+    Event.preventDefault();
+    var x=document.forms["contactform"]["email"].value;
+
+    if (x=="") {
+        alert("email must be filled out");
+        return false;
+
+    }else{
+        $(this),slideUp();
+        $("#success").slidedown();
+        //alert('Thank you for submitting the form');
