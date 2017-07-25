7fca10240772881f2070a406f6974444679abb36
ronniestovall <ronnie.stovall1996@gmail.com>
2017-07-25 14:00:14 -0500

    update components and styles

--- src/app.module.js
+++ src/app.module.js
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NavigationComponent } from './components/navigation/navigation.component';
 
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { ContactFormComponent } from './components/contactForm/contactForm.component';
 
 @NgModule({
     imports: [BrowserModule],
-    declarations: [NavigationComponent],
-    bootstrap:[NavigationComponent]
+    declarations: [NavigationComponent, ContactFormComponent],
+    bootstrap:[NavigationComponent, ContactFormComponent]
 })
 
 export class AppModule {}
